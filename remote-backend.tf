d363d7b2947572a77c73ae120cc5452a9d97524b
Dkflora <dkflora13@gmail.com>
2025-09-13 20:03:42 -0700

    dev infrastructure pipeline

---
+++ remote-backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket       = "makasiw7backet"
+    key          = "demodocker1/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = false
+  }
+}
